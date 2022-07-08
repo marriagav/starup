c3b1027b0f7909192867c44f3480a16baa7d38f5
Miguel Arriaga Velasco <marriagav@fb.com>
2022-07-08 10:41:43 -0700

    backend skeleton for HomeViewcontroller

--- starup/starup/View Controllers/HomeViewController.m
+++ starup/starup/View Controllers/HomeViewController.m
@@ -35,6 +35,79 @@
     }];
 }
 
+- (void)refreshDataWithNPosts:(int) numberOfPosts{
+//    Refreshes the tableview data with numberOfPosts posts
+    // construct query
+    
+
+    // fetch data asynchronously
+    
+}
+
+- (void)_beginRefresh:(UIRefreshControl *)refreshControl {
+//    Refreshes the data using the UIRefreshControl
+    // construct query
+    
+
+    // fetch data asynchronously
+    
+}
+
+
+- (void)_initializeRefreshControl{
+//    Initialices and inserts the refresh control
+    
+    
+}
+
+- (void)_initializeRefreshControlB{
+//    Initialices and inserts the refresh control
+    // Set up Infinite Scroll loading indicator
+   
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:
+    (NSInteger)section{
+//    return amount of posts in the postArray
+    
+    
+    return 0;
+    }
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:
+    (NSIndexPath *)indexPath{
+//    initialize cell (PostCell) to a reusable cell using the PostCell identifier
+    
+//    get the post and delegate and assign it to the cell
+    
+    return NULL;
+    
+}
+
+- (void)didPost{
+//    Gets called when the user presses the "share" button on the "ComposePost" view, this controller functions as a delegate of the former
+    
+}
+
+- (void)tableView:(UITableView *)tableView willDisplayCell:(UITableViewCell *)cell forRowAtIndexPath:(NSIndexPath *)indexPath{
+//    Calls load more data when scrolling reaches bottom of the tableView
+    
+}
+
+- (void)scrollViewDidScroll:(UIScrollView *)scrollView {
+//    Checks if is scrolling
+}
+
+- (void)loadMoreData{
+//    Adds 20 more posts to the tableView, for infinte scrolling
+    
+}
+
+- (void)postCell:(PostCell *)postCell didTap:(PFUser *)user{
+//    Performs view change when the user taps on a profile image
+    
+}
+
 /*
 #pragma mark - Navigation
 
---
+++ starup/starup/Views/PostCell.m
@@ -0,0 +1,23 @@
+//
+//  PostCell.m
+//  starup
+//
+//  Created by Miguel Arriaga Velasco on 7/8/22.
+//
+
+#import "PostCell.h"
+
+@implementation PostCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
