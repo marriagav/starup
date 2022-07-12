e40a0cdb492af00e2aea4b1a593230858398f39b
Miguel Arriaga Velasco <marriagav@fb.com>
2022-07-12 15:55:07 -0700

    starups now appear on the starups tab

--- starup/starup/Views/StarupCell.h
+++ starup/starup/Views/StarupCell.h
@@ -6,10 +6,26 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "Parse/Parse.h"
+#import "Starup.h"
+#import "DateTools.h"
+#import "Algos.h"
 
 NS_ASSUME_NONNULL_BEGIN
 
+@import Parse;
+
 @interface StarupCell : UITableViewCell
+
+@property (weak, nonatomic) IBOutlet UILabel *starupName;
+@property (weak, nonatomic) IBOutlet UILabel *starupCategory;
+@property (weak, nonatomic) IBOutlet UILabel *operatingSince;
+@property (weak, nonatomic) IBOutlet UILabel *sales;
+@property (weak, nonatomic) IBOutlet PFImageView *starupImage;
+@property (weak, nonatomic) IBOutlet UILabel *starupDescription;
+@property (strong, nonatomic) Starup *starup;
+
+- (void)setStarup:(Starup *)starup;
 
 @end
 
