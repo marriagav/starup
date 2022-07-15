6eed377522d0b6b7a5cacc48d70f93e969cce9f6
Miguel Arriaga Velasco <marriagav@fb.com>
2022-07-15 12:32:58 -0700

    starup images on profiles now display correclty depending on user role in that starup

--- starup/starup/Views/starupCollectionViewCell.h
+++ starup/starup/Views/starupCollectionViewCell.h
@@ -12,10 +12,10 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface starupCollectionViewCell : UICollectionViewCell
 
-@property (weak, nonatomic) IBOutlet PFImageView *userRoleImage;
 @property (weak, nonatomic) IBOutlet UILabel *starupName;
 @property (strong, nonatomic) Collaborator *collaborator;
 @property (strong, nonatomic) NSString *userRoleText;
+@property (weak, nonatomic) IBOutlet UIImageView *userRoleImage;
 
 - (void)setCollaborator:(Collaborator *)collaborator;
 
--- starup/starup/Views/starupCollectionViewCell.m
+++ starup/starup/Views/starupCollectionViewCell.m
@@ -19,6 +19,26 @@
     _collaborator = collaborator;
     self.starupName.text = self.collaborator[@"starup"][@"starupName"];
     self.userRoleText = self.collaborator[@"typeOfUser"];
+    [self setImageFromText];
+}
+
+- (void)setImageFromText{
+    if ([self.userRoleText isEqual:@"Ideator"]){
+        //  Set the profile picture
+        self.userRoleImage.image = [UIImage imageNamed:@"ideator-1"];
+    }
+    else if ([self.userRoleText isEqual:@"Shark"]){
+        //  Set the profile picture
+        self.userRoleImage.image = [UIImage imageNamed:@"shark-1"];
+    }
+    else if ([self.userRoleText isEqual:@"Hacker"]){
+        //  Set the profile picture
+        self.userRoleImage.image = [UIImage imageNamed:@"hacker-1"];
+    }
+    //    Format the profile picture
+    self.userRoleImage.layer.cornerRadius = self.userRoleImage.frame.size.height/2;
+    self.userRoleImage.layer.borderWidth = 0;
+    self.userRoleImage.clipsToBounds=YES;
 }
 
 @end
